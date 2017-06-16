bd2ae65608b369756b09fa08bd82e4310d909ce4
has85 <hasanov85@gmail.com>
2017-06-16 14:54:49 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by nurlan.hasanov on 16.06.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world, Loftblog");
+    }
+}
